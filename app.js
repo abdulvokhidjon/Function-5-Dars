a0699af2d7517f051817ce153ca8b25678519f68
abdulvokhidjon <162565383+abdulvokhidjon@users.noreply.github.com>
2024-04-27 08:52:06 +0500

    added

--- app.js
+++ app.js
@@ -171,3 +171,184 @@
 // }
 
 // thirdSide();
+
+// N{11}
+
+
+
+// function calculate(a, b) {
+//   return a % b;
+// }
+// console.log(calculate(1, 3));
+// console.log(calculate(-9, 45));
+// console.log(calculate(3, 4));
+
+// N{12}
+
+// const areaOfRectangle = (a, b) => {
+//   return a * b;
+// };
+// console.log(areaOfRectangle(3, 4));
+
+// N{13}
+
+// const addString = (a) => {
+//   return `Someting ${a}`;
+// };
+
+// console.log(addString("Teacher"));
+
+// N{14}
+
+// const sqr = (n) => {
+//   return n ** 2;
+// };
+// console.log(sqr(5));
+
+// N{15}
+
+// function zero(n) {
+//   if (n <= 0) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// zero(-8);
+
+// N{16}
+
+// function calculate(n) {
+//   return (n - 2) * 180;
+// }
+// console.log(calculate(4));
+
+// N{17}
+
+// function calculate(x, y) {
+//   return x * 2 + y * 3;
+// }
+// console.log(calculate(4, 2));
+
+// N{18}
+
+// const addString = (a) => {
+//   return `${a}Edabit`;
+// };
+// console.log(addString("Teacher"));
+
+// N{19}
+
+// const biggerThanHundred = function (a, b) {
+//   if (a + b < 100) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// };
+// biggerThanHundred(34, 45);
+// biggerThanHundred(34, 75);
+
+// N{20}
+
+// let newArray = [];
+// function array(n) {
+//   for (i = 1; i <= n; i++) {
+//     newArray.push(i);
+//   }
+// }
+// array(20);
+// console.log(newArray);
+
+// N{21}
+
+// function countLegs(tovuq, qoy, mol) {
+//   return tovuq * 2 + qoy * 4 + mol * 4;
+// }
+// console.log(countLegs(2, 3, 5));
+
+// N{22}
+
+// function bool(a, b) {
+//   if (a == true && b == false) {
+//     console.log(false);
+//   } else if (a == true && b == true) {
+//     console.log(true);
+//   } else if (a == false && b == true) {
+//     console.log(false);
+//   } else if (a == false && b == false) {
+//     console.log(false);
+//   }
+// }
+// bool(true, false);
+// bool(true, true);
+
+// N{23}
+
+// function isEqual(a, b) {
+//   if (a === b) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// isEqual(4, 4);
+// isEqual(4, 8);
+
+// N{24}
+
+// function points(win, draw, lost) {
+//   return win * 3 + draw * 1;
+// }
+
+// console.log(points(6, 2, 3));
+
+// N{25}
+
+// function secunds(h, m) {
+//   return h * 3600 + m * 60;
+// }
+
+// console.log(secunds(1, 2));
+
+// N{26}
+
+// function fun(n) {
+//   if (n === 7) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// fun(4);
+// fun(7);
+// fun(9);
+
+// N{27}
+
+// function isEqual(a, b) {
+//   if (a === b) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// isEqual(1, 1);
+// isEqual(1, "1");
+
+// N{28}
+
+// function funToString(bool) {
+//   return String(bool);
+// }
+// console.log(funToString(true));
+// console.log(typeof funToString(true));
+
+// N{29}
+
+// const arrowFunction = (v) => {
+//   return v;
+// };
+// console.log(arrowFunction(3));
+// console.log(arrowFunction("3"));
+// console.log(arrowFunction(true));
